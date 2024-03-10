0e2d66c1c52bbe7c372e5a32ecb6a3988ef2d254
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2024-03-09 21:20:39 -0600

    BJAO
    BJAO

---
+++ BJAO.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class BJAO{
+
+	protected: string nombres,apellidos,direccion;
+	int telefono;
+	
+
+	void crear ();
+	void leer ();
+	void actualizar ();
+	void borrar ();
+ 
+};
